ae90e605c3569fa3ff4957c76c9d55d74e3da7de
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-22 12:54:22 +0530

    fixing firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDW4-MdWAbOcH6rEWjBzJ8MRyVDzQMJulc",
-  authDomain: "netflixgpt-ac554.firebaseapp.com",
-  projectId: "netflixgpt-ac554",
-  storageBucket: "netflixgpt-ac554.firebasestorage.app",
-  messagingSenderId: "650119220392",
-  appId: "1:650119220392:web:1ee395cc6a28bf72a6469d",
-  measurementId: "G-2G6DVM8BXD",
+  apiKey: "AIzaSyDnR6j5pL21pAdSZync3X1eQrMsVYugSg8",
+  authDomain: "netflixgpt-e604a.firebaseapp.com",
+  projectId: "netflixgpt-e604a",
+  storageBucket: "netflixgpt-e604a.firebasestorage.app",
+  messagingSenderId: "906923327623",
+  appId: "1:906923327623:web:5c11af69cdd0f30900b822",
+  measurementId: "G-0QM56XHLLR",
 };
 
 // Initialize Firebase
